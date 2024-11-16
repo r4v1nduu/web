aa1f044a77730221b2349b0251fa509908075237
Ravindu Kavishka <r4v1nduu@gmail.com>
2024-11-16 18:48:10 +0530

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  output: 'export',
+  basePath: isProd ? '/web' : '',
+  assetPrefix: isProd ? '/web/' : '',
+};
+
 export default nextConfig;
